0007f59e3a0d245d61fab6ecb0a8eff11962e888
Alex Podeanu <podeanualexandru@hotmail.com>
2018-03-16 13:42:48 +0000

    Minify and Image optimisation

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var imagemin = require('gulp-imagemin');
 let cleanCSS = require('gulp-clean-css');
 
 gulp.task('styles', function() {
@@ -12,9 +13,16 @@ gulp.task('default',function() {
   gulp.watch('sass/**/*.sass',['styles']);
 });
 
-
+//Minify CSS
 gulp.task('minify-css', () => {
   return gulp.src('css/*.css')
     .pipe(cleanCSS({compatibility: 'ie8'}))
     .pipe(gulp.dest('dist'));
+});
+
+//Optimize images
+gulp.task('image', function() {
+  return gulp.src('img/logos/*')
+             .pipe(imagemin())
+             .pipe(gulp.dest('img/logos'));
 });
